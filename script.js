eacfa372f770b162453efafd6b632698f137636d
Mayonesa90 <mayonaas@hotmail.com>
2024-02-29 17:39:10 +0100

    adjusted logo animation

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+
+document.querySelector("body").addEventListener("load", () => {
+    console.log("Hej")
+})
+
 const linkedinBtn = document.querySelector(".linkedin"),
       githubBtn = document.querySelector(".github"),
       linkedinTxt = document.querySelector(".linkedin-selected"),
@@ -6,8 +11,12 @@ const linkedinBtn = document.querySelector(".linkedin"),
 
 linkedinBtn.addEventListener("mouseover", () => {
     linkedinTxt.style.display = "block";
+    linkedinBtn.style.animation = "tilt 2600ms"
 })
 
 githubBtn.addEventListener("mouseover", () => {
     githubTxt.style.display = "block";
+    githubBtn.style.animation = "tilt 2600ms";
+    githubBtn.style.cursor = "pointer"
 })
+
