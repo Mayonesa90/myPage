4078e402ff959fc2c879ac3b2c8d456eadae231e
Mayonesa90 <mayonaas@hotmail.com>
2024-02-29 18:46:55 +0100

    adjusted link to external sites

--- script.js
+++ script.js
@@ -11,12 +11,17 @@ const linkedinBtn = document.querySelector(".linkedin"),
 
 linkedinBtn.addEventListener("mouseover", () => {
     linkedinTxt.style.display = "block";
-    linkedinBtn.style.animation = "tilt 2600ms"
+    linkedinBtn.style.animation = "tilt 2600ms";
+    linkedinBtn.style.cursor = "pointer"
+    linkedinBtn.setAttribute("href", "https://www.linkedin.com/in/maya-arzapalo-björklund/")
+    linkedinBtn.setAttribute("target", "_blank");
 })
 
 githubBtn.addEventListener("mouseover", () => {
     githubTxt.style.display = "block";
     githubBtn.style.animation = "tilt 2600ms";
-    githubBtn.style.cursor = "pointer"
+    githubBtn.style.cursor = "pointer";
+    githubBtn.setAttribute("href", "https://github.com/Mayonesa90");
+    githubBtn.setAttribute("target", "_blank");
 })
 
