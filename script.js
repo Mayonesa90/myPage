cb4b11e14abcc090d241142c99d65bc232282603
Mayonesa90 <mayonaas@hotmail.com>
2024-03-01 12:26:27 +0100

    adjusted text slide animation

--- script.js
+++ script.js
@@ -11,18 +11,13 @@ const linkedinBtn = document.querySelector(".linkedin"),
 
 linkedinBtn.addEventListener("click", () => {
     linkedinTxt.className = linkedinTxt.className === "--selected" ? "--deselected" : "--selected";
-    // linkedinTxt.setAttribute("href", "https://linkedin.com/in/maya-arzapalo-björklund/");
-    // linkedinBtn.className = linkedinBtn.className === "--animation" ? "github" : "--animation";
-    // setTimeout((linkedinBtn.style.animation = "tilt 2600ms"), "2601");
     linkedinBtn.classList.add("--animation");
-    // setTimeout((linkedinBtn.classList.remove("--animation")), 5000);
     setTimeout(() => {
         linkedinBtn.classList.remove("--animation")
     }, 3000)
 })
 
 githubBtn.addEventListener("click", () => {
-    // githubBtn.style.animation = "tilt 2600ms";
     githubTxt.className = githubTxt.className === "--selected" ? "--deselected" : "--selected";
     githubBtn.classList.add("--animation");
     setTimeout(() => {
