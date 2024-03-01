6b6f23f326b6775d123346c08a326444aeb49d47
Mayonesa90 <mayonaas@hotmail.com>
2024-03-01 12:21:43 +0100

    adjusted logo animation

--- script.js
+++ script.js
@@ -5,31 +5,27 @@ document.querySelector("body").addEventListener("load", () => {
 
 const linkedinBtn = document.querySelector(".linkedin"),
       githubBtn = document.querySelector(".github"),
-      linkedinTxt = document.querySelector(".linkedin-selected"),
-      githubTxt = document.querySelector(".github-selected");
+      linkedinTxt = document.querySelector("#linkedin-link"),
+      githubTxt = document.querySelector("#github-link");
 
 
 linkedinBtn.addEventListener("click", () => {
-    linkedinTxt.style.display = "block";
-    linkedinBtn.style.animation = "tilt 2600ms";
-    linkedinBtn.style.cursor = "pointer";
-
-    linkedinBtn.addEventListener("click", () => {
-        linkedinBtn.setAttribute("href", "https://www.linkedin.com/in/maya-arzapalo-björklund/")
-        linkedinBtn.setAttribute("target", "_blank");
-    })
-    
+    linkedinTxt.className = linkedinTxt.className === "--selected" ? "--deselected" : "--selected";
+    // linkedinTxt.setAttribute("href", "https://linkedin.com/in/maya-arzapalo-björklund/");
+    // linkedinBtn.className = linkedinBtn.className === "--animation" ? "github" : "--animation";
+    // setTimeout((linkedinBtn.style.animation = "tilt 2600ms"), "2601");
+    linkedinBtn.classList.add("--animation");
+    // setTimeout((linkedinBtn.classList.remove("--animation")), 5000);
+    setTimeout(() => {
+        linkedinBtn.classList.remove("--animation")
+    }, 3000)
 })
 
 githubBtn.addEventListener("click", () => {
-    githubTxt.style.display = "block";
-    githubBtn.style.animation = "tilt 2600ms";
-    githubBtn.style.cursor = "pointer";
-
-    githubBtn.addEventListener("click", () => {
-        githubBtn.setAttribute("href", "https://github.com/Mayonesa90");
-        githubBtn.setAttribute("target", "_blank");
-    })
-    
+    // githubBtn.style.animation = "tilt 2600ms";
+    githubTxt.className = githubTxt.className === "--selected" ? "--deselected" : "--selected";
+    githubBtn.classList.add("--animation");
+    setTimeout(() => {
+        githubBtn.classList.remove("--animation")
+    }, 3000)
 })
-
