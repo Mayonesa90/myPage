bc034f84425e5fd688bf08c7e73947c015fb9ecf
Mayonesa90 <mayonaas@hotmail.com>
2024-03-02 17:58:47 +0100

    adjusted z-index and class names

--- script.js
+++ script.js
@@ -3,39 +3,19 @@ document.querySelector("body").addEventListener("load", () => {
     console.log("Hej")
 })
 
-const linkedinBtn = document.querySelector(".linkedin"),
-      githubBtn = document.querySelector(".github"),
-    //   linkedinTxt = document.querySelector("#linkedin-link"),
-    //   githubTxt = document.querySelector("#github-link");
+const linkedinBtn = document.querySelector(".btn_linkedin"),
+      githubBtn = document.querySelector(".btn_github"),
       linkedinTxt = document.createElement("a"),
       githubTxt = document.createElement("a"),
       linkedInParent = document.querySelector(".linkedIn_parent"),
       gitHubParent = document.querySelector(".gitHub_parent");
 
 
-// linkedinBtn.addEventListener("click", () => {
-//     linkedinTxt.className = linkedinTxt.className === "--selected" ? "--deselected" : "--selected";
-//     linkedinBtn.classList.add("--animation");
-//     setTimeout(() => {
-//         linkedinBtn.classList.remove("--animation")
-//     }, 3000)
-// })
-
-// githubBtn.addEventListener("click", () => {
-//     githubTxt.className = githubTxt.className === "--selected" ? "--deselected" : "--selected";
-//     githubBtn.classList.add("--animation");
-//     setTimeout(() => {
-//         githubBtn.classList.remove("--animation")
-//     }, 3000)
-// })
-
-
 linkedinBtn.addEventListener("click", () => {
     if (linkedInParent.children.length < 1) {
         linkedinTxt.setAttribute("href", "https://linkedin.com/in/maya-arzapalo-björklund");
         linkedinTxt.setAttribute("target", "_blank");
-        linkedinTxt.classList.add("card_text");
-        // linkedInParent.classList.add("--expand");
+        linkedinTxt.classList.add("--slidein");
         linkedinTxt.textContent = "https://linkedin.com/in/maya-arzapalo-björklund/";
         linkedInParent.appendChild(linkedinTxt);
         linkedinBtn.classList.add("--animation");
@@ -43,7 +23,7 @@ linkedinBtn.addEventListener("click", () => {
             linkedinBtn.classList.remove("--animation")
         }, 2500)
     } else {
-        linkedinTxt.classList.replace("card_text", "--slideout");
+        linkedinTxt.classList.replace("--slidein", "--slideout");
         setTimeout(() => {
             linkedInParent.removeChild(linkedinTxt);
         }, 2200);
@@ -59,7 +39,7 @@ githubBtn.addEventListener("click", () => {
     if (gitHubParent.children.length < 1) {
         githubTxt.setAttribute("href", "https://github.com/Mayonesa90");
         githubTxt.setAttribute("target", "_blank");
-        githubTxt.classList.add("card_text");
+        githubTxt.classList.add("--slidein");
         githubTxt.textContent = "https://github.com/Mayonesa90";
         gitHubParent.appendChild(githubTxt);
         githubBtn.classList.add("--animation");
@@ -67,7 +47,7 @@ githubBtn.addEventListener("click", () => {
                 githubBtn.classList.remove("--animation")
             }, 2500)
     } else {
-        githubTxt.classList.replace("card_text", "--slideout");
+        githubTxt.classList.replace("--slidein", "--slideout");
         setTimeout(() => {
             gitHubParent.removeChild(githubTxt)
         }, 2200);
